ff31dfd2c268fcb824fc96792616d1d5105cf9f3
BellPumpkin <whdgh135@gmail.com>
2024-04-02 23:42:30 +0900

    Design: 회원가입 폼 수정
    회원가입 시 아무 값도 입력하지 않고 "계정 등록하기" 버튼을 클릭하면 요소가 상위 요소를 넘어가는 문제를 해결하기 위해 height 크기를 늘렸습니다

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -4,7 +4,7 @@ export default function SignUpLayout({children}: Readonly<{ children: React.Reac
   return (
     <>
       <section className='flex w-full h-screen justify-center items-center'>
-        <div className='w-[550px] h-[650px] border border-neutral-300 rounded-md p-5'>
+        <div className='w-[550px] h-[820px] border border-neutral-300 rounded-md p-5'>
           <div className={'flex flex-col mb-5'}>
             <h3 className='text-2xl font-bold'>계정을 생성합니다.</h3>
             <p className='text-sm text-slate-600'>필수 정보를 입력해볼게요.</p>
